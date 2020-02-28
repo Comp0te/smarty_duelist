c1df77411936312f88f4b33d60055ae706991e27
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-02-28 17:39:09 +0200

    Remove UnknownFailure

--- lib/src/domain/core/models/failures.dart
+++ lib/src/domain/core/models/failures.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:meta/meta.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
 
@@ -8,5 +7,4 @@ part 'failures.freezed.dart';
 @freezed
 abstract class Failure with _$Failure {
   const factory Failure.auth(AuthException exception) = AuthFailure;
-  const factory Failure.unknown(Exception exception) = UnknownFailure;
 }
