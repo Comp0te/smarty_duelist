5f2b2abfe25fbf27272b249ae9d1d63864f32bc9
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-07 22:12:22 +0200

    Add outline mode to ios button

--- lib/src/presentation/shared_widgets/button.dart
+++ lib/src/presentation/shared_widgets/button.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
@@ -8,44 +10,64 @@ class Button extends StatelessWidget {
   final String title;
   final bool isLoading;
   final Color color;
-  final EdgeInsets margin;
+  final double height;
+  final bool iosOutlined;
 
   const Button({
     Key key,
-    this.onPress,
     @required this.title,
+    this.onPress,
     this.isLoading = true,
     this.color,
-    this.margin = const EdgeInsets.only(bottom: 25),
+    this.height = 44,
+    this.iosOutlined = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: margin,
-      width: double.infinity,
-      height: 44,
-      child: PlatformButton(
-        onPressed: isLoading ? null : onPress,
-        android: (_) => MaterialRaisedButtonData(
-          color: Theme.of(context).primaryColor,
-          disabledColor: Theme.of(context).disabledColor.withOpacity(0.1),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
+    final isOutlined =
+        iosOutlined && Platform.isIOS && !isLoading && onPress != null;
+    final borderRadius = BorderRadius.circular(16);
+    final primaryColor = color ?? Theme.of(context).primaryColor;
+
+    return PlatformButton(
+      onPressed: isLoading ? null : onPress,
+      padding: const EdgeInsets.all(0),
+      disabledColor: Theme.of(context).disabledColor.withOpacity(0.1),
+      color: primaryColor,
+      android: (_) => MaterialRaisedButtonData(
+        shape: RoundedRectangleBorder(
+          borderRadius: borderRadius,
         ),
-        ios: (_) => CupertinoButtonData(
-          minSize: 44,
-          color: Theme.of(context).primaryColor,
-          disabledColor: Theme.of(context).disabledColor.withOpacity(0.1),
-          borderRadius: BorderRadius.circular(16),
-        ),
+      ),
+      ios: (_) => CupertinoButtonData(
+        minSize: 44,
+        color: iosOutlined ? Theme.of(context).backgroundColor : null,
+        borderRadius: borderRadius,
+      ),
+      child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 8),
+        width: double.infinity,
+        height: height,
+        decoration: isOutlined
+            ? BoxDecoration(
+                borderRadius: borderRadius,
+                border: Border.all(
+                  color: primaryColor,
+                  width: 1,
+                ))
+            : null,
         child: isLoading
-            ? Center(child: Spinner(color: Theme.of(context).primaryColor))
+            ? Spinner(color: primaryColor)
             : PlatformText(
                 title,
+                maxLines: 1,
+                softWrap: false,
                 textAlign: TextAlign.center,
-                style: Theme.of(context).primaryTextTheme.button,
+                style: Theme.of(context).primaryTextTheme.button.copyWith(
+                      color: isOutlined ? primaryColor : null,
+                    ),
               ),
       ),
     );
