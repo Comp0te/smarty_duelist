cb4d1cce02563261420c9f0a2b8345736e9566d1
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-22 18:06:34 +0200

    Add BlocSupervisor and WidgetsFlutterBinding.ensureInitialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
+import 'src/presentation/presentation.dart' show App, SimpleBlocDelegate;
 import 'src/injector/injector.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   configureInjector();
   runApp(App());
 }
