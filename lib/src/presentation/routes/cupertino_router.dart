e4555857209545417aad7578c51b6f4d696fad34
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-22 18:05:09 +0200

    Add new routes

--- lib/src/presentation/routes/cupertino_router.dart
+++ lib/src/presentation/routes/cupertino_router.dart
@@ -1,11 +1,27 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:auto_route/auto_route_annotations.dart';
 
 import 'package:smarty_duelist/src/presentation/pages/pages.dart'
-    show SignInPage, SignUpPage;
+    show HomePage, SignInPage, SignUpPage, SplashPage;
+
+import 'guards/guards.dart';
 
 @CupertinoAutoRouter(generateNavigationHelperExtension: true)
 class $CupertinoRouter {
   @initial
+  SplashPage splashPage;
+  // Auth routes
+  @CustomRoute(
+    transitionsBuilder: TransitionsBuilders.fadeIn,
+    durationInMilliseconds: 500,
+  )
   SignInPage signInPage;
   SignUpPage signUpPage;
+  // App routes
+  @GuardedBy([AuthGuard])
+  @CustomRoute(
+    transitionsBuilder: TransitionsBuilders.fadeIn,
+    durationInMilliseconds: 500,
+  )
+  HomePage homePage;
 }
--- lib/src/presentation/routes/cupertino_router.gr.dart
+++ lib/src/presentation/routes/cupertino_router.gr.dart
@@ -7,15 +7,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:smarty_duelist/src/presentation/pages/splash_page/splash_page.dart';
 import 'package:smarty_duelist/src/presentation/pages/sign_in_page/sign_in_page.dart';
 import 'package:smarty_duelist/src/presentation/pages/sign_up_page/sign_up_page.dart';
+import 'package:smarty_duelist/src/presentation/pages/home-page/home_page.dart';
+import 'package:smarty_duelist/src/presentation/routes/guards/aurh_guard.dart';
 
 abstract class Routes {
-  static const signInPage = '/';
+  static const splashPage = '/';
+  static const signInPage = '/sign-in-page';
   static const signUpPage = '/sign-up-page';
+  static const homePage = '/home-page';
 }
 
 class CupertinoRouter extends RouterBase {
+  @override
+  Map<String, List<Type>> get guardedRoutes => {
+        Routes.homePage: [AuthGuard],
+      };
+
   //This will probably be removed in future versions
   //you should call ExtendedNavigator.ofRouter<Router>() directly
   static ExtendedNavigatorState get navigator =>
@@ -25,15 +35,30 @@ class CupertinoRouter extends RouterBase {
   Route<dynamic> onGenerateRoute(RouteSettings settings) {
     final args = settings.arguments;
     switch (settings.name) {
-      case Routes.signInPage:
+      case Routes.splashPage:
         return CupertinoPageRoute<dynamic>(
-          builder: (_) => SignInPage().wrappedRoute,
+          builder: (_) => SplashPage(),
           settings: settings,
+        );
+      case Routes.signInPage:
+        return PageRouteBuilder<dynamic>(
+          pageBuilder: (ctx, animation, secondaryAnimation) =>
+              SignInPage().wrappedRoute,
+          settings: settings,
+          transitionsBuilder: TransitionsBuilders.fadeIn,
+          transitionDuration: const Duration(milliseconds: 500),
         );
       case Routes.signUpPage:
         return CupertinoPageRoute<dynamic>(
           builder: (_) => SignUpPage().wrappedRoute,
           settings: settings,
+        );
+      case Routes.homePage:
+        return PageRouteBuilder<dynamic>(
+          pageBuilder: (ctx, animation, secondaryAnimation) => HomePage(),
+          settings: settings,
+          transitionsBuilder: TransitionsBuilders.fadeIn,
+          transitionDuration: const Duration(milliseconds: 500),
         );
       default:
         return unknownRoutePage(settings.name);
@@ -46,6 +71,8 @@ class CupertinoRouter extends RouterBase {
 //***************************************************************************
 
 extension CupertinoRouterNavigationHelperMethods on ExtendedNavigatorState {
+  Future pushSplashPage() => pushNamed(Routes.splashPage);
   Future pushSignInPage() => pushNamed(Routes.signInPage);
   Future pushSignUpPage() => pushNamed(Routes.signUpPage);
+  Future pushHomePage() => pushNamed(Routes.homePage);
 }
