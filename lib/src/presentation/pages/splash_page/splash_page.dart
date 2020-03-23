49810049cab820534bf3e8e8f7d145f61a6425ff
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-23 14:23:41 +0200

    Remove AuthBlocListener from SplashPage

--- lib/src/presentation/pages/splash_page/splash_page.dart
+++ lib/src/presentation/pages/splash_page/splash_page.dart
@@ -3,17 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:smarty_duelist/src/presentation/shared_widgets/shared_widgets.dart'
     show NativeScaffold, Spinner, SpinnerMode;
 
-import 'widgets/widgets.dart';
-
 class SplashPage extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
-    return const AuthBlocListener(
-      child: NativeScaffold(
-        body: Spinner(spinnerMode: SpinnerMode.standalone),
-      ),
+    return const NativeScaffold(
+      body: Spinner(spinnerMode: SpinnerMode.standalone),
     );
   }
 }
