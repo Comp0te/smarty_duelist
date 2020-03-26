b3aa6fea4f09799dda124fb591a346195b2e6d65
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-26 16:53:02 +0200

    Add NativeAlert widget

---
+++ lib/src/presentation/shared_widgets/native_alert.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+import 'native_dialog_action.dart';
+
+class NativeAlert extends StatelessWidget {
+  final String title;
+  final Widget content;
+  final List<NativeDialogAction> actions;
+
+  const NativeAlert({
+    @required this.title,
+    @required this.content,
+    @required this.actions,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformAlertDialog(
+      title: Text(title),
+      content: content,
+      ios: (_) => CupertinoAlertDialogData(),
+      android: (_) => MaterialAlertDialogData(),
+      actions: actions,
+    );
+  }
+}
