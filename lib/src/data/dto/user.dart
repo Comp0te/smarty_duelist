eaebfc847affbdb65d224478d9fc04471ee25144
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-28 00:02:24 +0200

    Add User dto

---
+++ lib/src/data/dto/user.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:smarty_duelist/src/domain/auth/auth.dart';
+
+extension FirebaseUserX on FirebaseUser {
+  User toDomainUser() {
+    return this != null
+        ? User(
+            id: uid,
+            name: displayName ?? email.split('@').first,
+            email: email,
+            photoUrl: photoUrl,
+            creationTimestamp: metadata.creationTime,
+            lastSignInTimestamp: metadata.lastSignInTime,
+          )
+        : null;
+  }
+}
