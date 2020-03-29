bf1780318d83f6f0b3ede463cd20b0b1691e7689
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-29 20:39:31 +0300

    Configure CustomBlocDelegate for HydratedBlocDelegate

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
-import 'src/presentation/presentation.dart' show App, SimpleBlocDelegate;
+import 'src/presentation/presentation.dart' show App, CustomBlocDelegate;
 import 'src/injector/injector.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  BlocSupervisor.delegate = SimpleBlocDelegate();
+  BlocSupervisor.delegate = await CustomBlocDelegate.build();
   configureInjector();
   runApp(App());
 }
