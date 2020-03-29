0f654bc2b75677ed79f47c897239715f819f6b18
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-03-29 20:39:31 +0300

    Configure CustomBlocDelegate for HydratedBlocDelegate

--- lib/src/presentation/core_blocs/bloc_delegate.dart
+++ lib/src/presentation/core_blocs/bloc_delegate.dart
@@ -1,7 +1,19 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/foundation.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
-class SimpleBlocDelegate extends BlocDelegate {
+class CustomBlocDelegate extends HydratedBlocDelegate {
+  CustomBlocDelegate._(HydratedStorage storage) : super(storage);
+
+  static Future<HydratedBlocDelegate> build() async {
+    return CustomBlocDelegate._(
+      await HydratedBlocStorage.getInstance(
+        storageDirectory: await getApplicationDocumentsDirectory(),
+      ),
+    );
+  }
+
   @override
   void onEvent(Bloc bloc, Object event) {
     super.onEvent(bloc, event);
