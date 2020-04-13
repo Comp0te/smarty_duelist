2c008c6f94951bd1eccafba9b7f63b84f63ae99a
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-04-13 16:40:40 +0300

    Configure avatar usage on profile_tab

--- lib/src/presentation/pages/profile_tab/profile_tab.dart
+++ lib/src/presentation/pages/profile_tab/profile_tab.dart
@@ -1,40 +1,54 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:smarty_duelist/generated/l10n.dart' show S;
-import 'package:smarty_duelist/src/injector/injector.dart' show getIt;
+import 'package:smarty_duelist/src/presentation/core_blocs/core_blocs.dart';
 
-import '../../shared_widgets/shared_widgets.dart' show Button;
+import '../../shared_widgets/shared_widgets.dart'
+    show Avatar, Button, showImageActions;
 import 'blocs/blocs.dart';
 
-class ProfileTab extends StatelessWidget
-    implements AutoRouteWrapper {
+export 'blocs/blocs.dart';
+
+class ProfileTab extends StatelessWidget {
   const ProfileTab();
 
   @override
-  Widget get wrappedRoute => BlocProvider<SignOutBloc>(
-        create: (_) => getIt<SignOutBloc>(),
-        child: this,
-      );
-
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          SizedBox(
-            width: 100,
-            child: Button(
-              title: S.of(context).logout,
-              onPress: () {
-                BlocProvider.of<SignOutBloc>(context).add(const SignOut());
-              },
-            ),
-          )
-        ],
-      ),
+    final profileTabBloc = BlocProvider.of<ProfileTabBloc>(context);
+    final url = BlocProvider.of<AuthBloc>(context).state.maybeWhen(
+          authAuthenticated: (user) => user.photoUrl,
+          orElse: () => null,
+        );
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Avatar(
+            size: 150,
+            url: url,
+            onTap: () {
+              showImageActions(
+                context,
+                profileTabBloc.imagePickerBloc,
+                url: url,
+              );
+            },
+          ),
+        ),
+        SizedBox(
+          width: 100,
+          child: Button(
+            title: S.of(context).logout,
+            onPress: () {
+              BlocProvider.of<SignOutBloc>(context).add(const SignOut());
+            },
+          ),
+        )
+      ],
     );
   }
 }
