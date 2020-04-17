e29f16f3d30dfa2ac754f3dec8104d6baaaa336c
Dmytro Vynnyk <dmytro.v@avocadotechnology.com>
2020-04-17 23:33:50 +0300

    Add LinearProgressIndicator to Spinner

--- lib/src/presentation/shared_widgets/spinner.dart
+++ lib/src/presentation/shared_widgets/spinner.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
+import '../theme/theme.dart';
+
 enum SpinnerMode { inner, standalone }
 
 class Spinner extends StatelessWidget {
   final SpinnerMode spinnerMode;
   final Color color;
+  final double progressValue;
 
   const Spinner({
     this.spinnerMode = SpinnerMode.inner,
     this.color,
+    this.progressValue,
   });
 
   @override
   Widget build(BuildContext context) {
+    if (progressValue != null) {
+      return LinearProgressIndicator(
+        value: progressValue,
+        backgroundColor: color ?? context.primaryColor,
+      );
+    }
+
     switch (spinnerMode) {
       case SpinnerMode.standalone:
         return Center(
